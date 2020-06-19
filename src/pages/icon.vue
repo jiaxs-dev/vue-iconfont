78250d7f3fa848e21d5ce60dfdcdbf2c5d3eb7cf
jiaxs@cloudscope.cn <jiaxs@cloudscope.cn>
2020-06-19 15:10:29 +0800

    icon样例

---
+++ src/pages/icon.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <i class="ali-icon-diangan"></i>
+    <i class="ali-icon-tijiao"></i>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "icon"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+import icon from "../pages/icon";
+
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/icon',
+      name: 'icon',
+      component: icon
     }
   ]
 })
